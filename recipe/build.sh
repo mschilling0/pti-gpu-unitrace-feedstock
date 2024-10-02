d734569775344f06fff356949f4d6159fe40a39d
Yevhenii Havrylko <yevhenii.havrylko@intel.com>
2024-10-02 12:23:20 -0400

    Unitrace clean up

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,15 +2,27 @@
 
 set -euxo pipefail
 
-cmake \
+if [ "$PKG_NAME" == "pti-gpu-unitrace" ]; then
+    SRC_DIR=./tools/unitrace
+    BLD_DIR=./tools/unitrace/build
+    CMAKE_ARGS="-DBUILD_WITH_MPI=OFF"
+else
+    SRC_DIR=./sdk
+    BLD_DIR=./sdk/build
+    CMAKE_ARGS="-DPTI_BUILD_TESTING=OFF -DPTI_BUILD_SAMPLES=OFF"
+fi
+
+cmake $CMAKE_ARGS \
     -G Ninja \
     -DCMAKE_C_COMPILER=icx \
     -DCMAKE_CXX_COMPILER=icpx \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_VERBOSE_MAKEFILE=ON \
-    -DPTI_BUILD_TESTING=OFF \
-    -DPTI_BUILD_SAMPLES=OFF \
     -DCMAKE_INSTALL_PREFIX:STRING=${PREFIX} \
-    -S ./sdk \
-    -B ./sdk/build 
-cmake --build ./sdk/build
+    -S ${SRC_DIR} \
+    -B ${BLD_DIR} 
+cmake --build ${BLD_DIR}
+
+if [ "$PKG_NAME" == "pti-gpu-unitrace" ]; then
+    cmake --install ${BLD_DIR} --prefix=${PREFIX}
+fi
