2098c5029d3bf678516a14713cc8267c50c42f55
Yevhenii Havrylko <egavrilko@gmail.com>
2024-11-08 16:47:43 -0500

    Fix windows build and improve meta

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,16 +1,16 @@
 SETLOCAL EnableDelayedExpansion
 
+pushd "%SRC_DIR%" || exit /b !ERRORLEVEL!
+
 if "%PKG_NAME%" == "pti-gpu-unitrace" (
-  set SRC_DIR=.\tools\unitrace
-  set BLD_DIR=.\tools\unitrace/build
+  set SRC_DIR=".\tools\unitrace"
+  set BLD_DIR=".\tools\unitrace\build"
   set CMAKE_ARGS=""
 ) else (
-  set SRC_DIR=.\sdk
-  set BLD_DIR=.\sdk\build
+  set SRC_DIR=".\sdk"
+  set BLD_DIR=".\sdk\build"
   set CMAKE_ARGS="-DPTI_BUILD_TESTING=OFF -DPTI_BUILD_SAMPLES=OFF"
 )
-
-pushd "%SRC_DIR%" || exit /b !ERRORLEVEL!
 
 :: Configure.
 cmake %CMAKE_ARGS% ^
