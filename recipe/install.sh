4b91eed12d1afb7d98c710e5db7396d1c8053b2f
Yevhenii Havrylko <yevhenii.havrylko@intel.com>
2024-11-09 18:06:36 +0000

    Clean up install script

--- recipe/install.sh
+++ recipe/install.sh
@@ -2,7 +2,5 @@
 
 set -euxo pipefail
 
-# cmake --install ./sdk/build --component Pti_Runtime --prefix=${PREFIX}
-# cmake --install ./sdk/build --component Pti_Development --prefix=${PREFIX}
-
-cmake --install ./sdk/build --prefix=${PREFIX}
+cmake --install ./sdk/build --component Pti_Runtime --prefix=${PREFIX}
+cmake --install ./sdk/build --component Pti_Development --prefix=${PREFIX}
